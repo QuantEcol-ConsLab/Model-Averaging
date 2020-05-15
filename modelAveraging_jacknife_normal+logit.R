7f1bf4a4abf5ca86e2a95aa0850877137015cf81
Mark <marks6@u.washington.edu>
2020-05-14 19:38:11 -0700

    First draft of jackknife model averaging

---
+++ modelAveraging_jacknife_normal+logit.R
@@ -0,0 +1,110 @@
+### Converse/Gardner Lab Meeting
+### 18 May 2020
+### Model Averaging Exercise (Dormann et al. 2018)
+
+# Set working directory & load libraries
+setwd("C:/Users/ameli/Dropbox/UW SAFS/Converse Lab/Lab Meetings/2020_Spring/Week 8")
+library(MuMIn)
+
+# Read-in data
+normdat<-read.csv("normalSimulatedData1April2020.csv")[,2:10]
+logitdat<-read.csv("logitSimulatedData1April2020.csv")[,2:10]
+
+## Normal data ##
+# Fit the global model
+fm1.norm <- glm(y~., data = normdat, na.action = "na.fail", family = "gaussian")
+
+# Use dredge to fit all possible models
+dfm1.norm.jack <- dredge(fm1.norm, rank="AIC") # do these need to be ranked?
+
+# Get all of the model fit results
+fm1mods.norm <- get.models(dfm1.norm.jack, subset = TRUE)
+
+# Jackknife 
+# The jackknife model averaging optimises the fit of the prediction onto an omitted data point. 
+# It is in a way similar to stacking, but requires only N steps (N = number of data points).
+# Code adapted from Dormann et al. 2018
+
+# Fit the candidate models, omitting one data point at a time.
+N <- nrow(normdat)
+M <- length(fm1mods.norm)
+train <- normdat # we will use the entire dataset here, rather than a subset
+J <- matrix(NA, N, M) # matrix with jackknifed predictions
+for (i in 1:N){
+  # re-fit models on train with one less data point:
+  jfits <- lapply(fm1mods.norm, update, .~. , data=train[-i,]) 
+  # predict them to omitted data point:
+  J[i,] <- sapply(jfits, predict, newdata=train[i, , drop=F]) 
+  rm(jfits)
+}
+
+# Compute RMSE for a value of w, given J.
+weightsopt <- function(ww, J){ 
+  # function to compute RMSE on test data
+  # at some point to also use likelihood instead of RMSE, but primarily for 0/1 data
+  w <- c(1, exp(ww)); w <- w/sum(w) 
+  Jpred <- J %*% w
+  return(sqrt(mean((Jpred - train$y)^2)))
+}
+ops <- optim(par=runif(NCOL(J)-1), weightsopt, method="BFGS", control=list(maxit=5000), J=J)
+if (ops$convergence != 0) stop("Not converged!")
+round(weightsJMA <- c(1, exp(ops$par))/sum(c(1, exp(ops$par))),3)
+sum(weightsJMA) # check to see if sums to 1
+weightedPredsJMA <- J %*% weightsJMA
+(RMSEjma <- sqrt(mean((weightedPredsJMA - train$y)^2)))
+
+# Plot predictions versus true data
+truth <-normdat$y
+preds <- weightedPredsJMA
+plot(x = preds, y = truth, xlab = "Predicted", ylab = "Observations")
+abline(0,1)
+    
+
+## Logit Data ##
+# Fit the global model
+fm1.logit <- glm(y~., data = logitdat, na.action = "na.fail", family = "binomial")
+
+# Use dredge to fit all possible models
+dfm1.logit.jack <- dredge(fm1.logit, rank="AIC") # do these need to be ranked?
+
+# Get all of the model fit results
+fm1mods.logit <- get.models(dfm1.logit.jack, subset = TRUE)
+
+# Jackknife 
+# The jackknife model averaging optimises the fit of the prediction onto an omitted data point. 
+# It is in a way similar to stacking, but requires only N steps (N = number of data points).
+# Code adapted from Dormann et al. 2018
+
+# Fit the candidate models, omitting one data point at a time.
+N <- nrow(logitdat)
+M <- length(fm1mods.logit)
+train <- logitdat # we will use the entire dataset here, rather than a subset
+J <- matrix(NA, N, M) # matrix with jackknifed predictions
+for (i in 1:N){
+  # re-fit models on train with one less data point:
+  jfits <- lapply(fm1mods.logit, update, .~. , data=train[-i,]) 
+  # predict them to omitted data point:
+  J[i,] <- sapply(jfits, predict, newdata=train[i, , drop=F]) 
+  rm(jfits)
+}
+
+# Compute RMSE for a value of w, given J.
+weightsopt <- function(ww, J){ 
+  # function to compute RMSE on test data
+  # at some point to also use likelihood instead of RMSE, but primarily for 0/1 data
+  w <- c(1, exp(ww)); w <- w/sum(w) 
+  Jpred <- J %*% w
+  return(sqrt(mean((Jpred - train$y)^2)))
+}
+ops <- optim(par=runif(NCOL(J)-1), weightsopt, method="BFGS", control=list(maxit=5000), J=J)
+if (ops$convergence != 0) stop("Not converged!")
+round(weightsJMA <- c(1, exp(ops$par))/sum(c(1, exp(ops$par))),3)
+
+weightedPredsJMA <- J %*% weightsJMA
+(RMSEjma <- sqrt(mean((weightedPredsJMA - train$y)^2)))
+
+# Plot predictions versus true data
+truth <-logitdat$y
+preds <- weightedPredsJMA
+plot(x = preds, y = truth, xlab = "Predicted", ylab = "Observations")
+abline(0,1)
